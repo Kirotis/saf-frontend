b052062b6fdbe1f5aed319de983cd7ccc6064af4
Kirill Titov <ktitov@aorti.ru>
2021-11-09 17:58:23 +0300

    init files

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-app id="app">
+    <v-app-bar max-height="68px" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>SAF</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-navigation-drawer temporary absolute v-model="drawer">
+        <v-list nav dense>
+          <v-list-item-group no-action v-model="group">
+
+            <router-link class="nuxt-link-custom" to="/">
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Home page</v-list-item-title>
+              </v-list-item>
+            </router-link>
+
+            <router-link class="nuxt-link-custom" to="/to-do">
+              <v-list-item eager>
+                <v-list-item-icon>
+                  <v-icon>mdi-checkbox-marked-circle-plus-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>To do page</v-list-item-title>
+              </v-list-item>
+            </router-link>
+
+            <router-link class="nuxt-link-custom" to="/login">
+              <v-list-item eager>
+                <v-list-item-icon>
+                  <v-icon>mdi-account</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Login</v-list-item-title>
+              </v-list-item>
+            </router-link>
+
+            <router-link class="nuxt-link-custom" to="/register">
+              <v-list-item eager>
+                <v-list-item-icon>
+                  <v-icon>mdi-account-plus</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Register</v-list-item-title>
+              </v-list-item>
+            </router-link>
+
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+      <router-view />
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: "default",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+.theme--dark.v-list-item--active:hover::before,
+.theme--dark.v-list-item--active::before {
+  opacity: none; 
+}
+.v-application a {
+  color: #fff !important;
+}
+.nuxt-link-custom {
+  text-decoration: none;
+}
+.layout__header {
+  justify-content: space-around;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-row class="home">
+    <v-hover v-slot="{ hover }">
+      <v-col class="indigo lighten-3 justify-center d-flex" cols="12" xs="12" sm="6">
+        <v-expand-transition>
+          <div
+            v-if="hover" 
+            class="
+              d-flex
+              transition-fast-in-fast-out
+              indigo lighten-2
+              text-h2
+              white--text
+              
+            "
+            style="height: 200px"
+          >
+            $14.99
+          </div>
+        </v-expand-transition>
+      </v-col>
+    </v-hover>
+    <v-hover v-slot="{ hover }">
+      <v-col class="purple lighten-3 justify-center d-flex" cols="12" xs="12" sm="6">
+        <v-expand-transition>
+          <div
+            v-if="hover" 
+            class="
+              d-flex
+              transition-fast-in-fast-out
+              purple lighten-2
+              text-h2
+              white--text
+            "
+            style="height: 200px"
+          >
+            $14.99
+          </div>
+        </v-expand-transition>
+      </v-col>
+    </v-hover>
+  </v-row>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  height: 100%;
+  margin: 0px;
+}
+</style>
